c87c700fad19bffbfc0b2d4cb6a36ff3ec824671
simonsajji <simonmathai777@gmail.com>
2022-03-13 19:50:22 +0530

    Updates

---
+++ precache-manifest.5f95c3545f4395a29db3ac73dcdbf3b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6b80548ef282d2f5c38",
+    "url": "/test-cake/static/css/main.13d28647.chunk.css"
+  },
+  {
+    "revision": "b6b80548ef282d2f5c38",
+    "url": "/test-cake/static/js/main.b6b80548.chunk.js"
+  },
+  {
+    "revision": "709d58a229027795f766",
+    "url": "/test-cake/static/js/1.709d58a2.chunk.js"
+  },
+  {
+    "revision": "169bc872531a3d6c4f6c",
+    "url": "/test-cake/static/js/2.169bc872.chunk.js"
+  },
+  {
+    "revision": "a52c1ba22ab77eb7bb5b",
+    "url": "/test-cake/static/js/runtime~main.a52c1ba2.js"
+  },
+  {
+    "revision": "b0b928c8e26d20a0b45a0f0eb80e190a",
+    "url": "/test-cake/static/media/PngItem_2473598.b0b928c8.png"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/test-cake/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "77f267bc2df487659c152126b86e43dc",
+    "url": "/test-cake/index.html"
+  }
+];
